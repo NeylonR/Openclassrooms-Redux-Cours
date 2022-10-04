681bf317db98d78902e9b2198b38bd49f3118a39
NeylonR <rodrigues.neylon.0@gmail.com>
2022-10-04 16:14:41 +0200

    exercice 2 part 2

--- src/Display.js
+++ src/Display.js
@@ -6,9 +6,11 @@ export default function Display() {
   const player2Score = useSelector((state) => state.player2Score);
   const advantage = useSelector((state) => state.advantage);
   const winner = useSelector((state) => state.winner);
+  const history = useSelector((state) => state.history)
   const winnerName = winner === "player1Score" ? "Player 1" : "Player 2";
 
   return (
+  <>
   <p className="display">
     {winner !== null ? winnerName : gameIsPlaying ? `${player1Score} - ${player2Score}` : "Game is paused"}
     <br/>
@@ -16,5 +18,11 @@ export default function Display() {
       (advantage === "player1Score" && `Player 1 has advantage`) || (advantage === "player2Score" && `Player 2 has advantage`)
     )}
   </p>
+  <div>
+    {history.length > 0 && history.map((score, index) => {
+      return <p key={score + index}>{score}</p>
+    })}
+  </div>
+  </>
   )
 }
--- src/Store.js
+++ src/Store.js
@@ -1,20 +1,26 @@
 import { createStore } from "redux";
 // import { configureStore } from '@reduxjs/toolkit'
+import produce from "immer";
 
 const initialState = {
   player1Score: 0,
   player2Score: 0,
   advantage: null,
   winner: null,
-  playing: true
+  playing: true,
+  history: []
 }
 
 function reducer(state = initialState, action) {
+  if(state.winner && action.type !== "restart"){
+    return state;
+  }
+
   if(action.type === "playPauseAction") {
-    return {
-      ...state,
-      playing: !state.playing
-    }
+    const nextState = produce(state, draft => {
+      draft.playing = !draft.playing;
+    })
+    return nextState;
   }
   
   if(action.type === "addPlayerScore") {
@@ -22,41 +28,58 @@ function reducer(state = initialState, action) {
     const otherPlayer = player === "player1Score" ? "player2Score" : "player1Score";
     const currentPlayerScore = state[player];
     
-    if(state.winner || state.playing === false){
+    if(state.playing === false){
       return state;
     }
     if(currentPlayerScore <= 15){
-        return {
-        ...state,
-        [player]: currentPlayerScore+ 15
-      }
+      const nextState = produce(state, draft => {
+        draft[player] = currentPlayerScore + 15;
+      })
+      return nextState;
     }
     if(currentPlayerScore === 30){
-        return {
-        ...state,
-          [player]: currentPlayerScore + 10
-      }
+      const nextState = produce(state, draft => {
+        draft[player] = currentPlayerScore + 10;
+      })
+      return nextState;
     }
     if(currentPlayerScore === 40){
-      console.log(player)
       if(state[otherPlayer] !== 40 || state.advantage === player){
-        return { 
-          ...state,
-          winner: player
-        }
+        const nextState = produce(state, draft => {
+          draft.winner = player;
+        })
+        return nextState;
       }
-      return {
-        ...state,
-        advantage: player
-      }
+      const nextState = produce(state, draft => {
+        draft.advantage = player;
+      })
+      return nextState;
     }
   }
   
   if(action.type === "restart"){
-    return initialState ;
+    const nextState = produce(state, draft => {
+      if(state.winner) {
+        const player = draft.winner;
+        const playerString = draft.winner === "player1Score" ? "Player 1 " : "Player 2 ";
+        const otherPlayer = draft.winner === "player1Score" ? "player2Score" : "player1Score";
+        draft.history.push(`${playerString} won ${draft[player]} - ${draft[otherPlayer]}`);
+      }
+      draft.player1Score = 0;
+      draft.player2Score = 0;
+      draft.advantage = null;
+      draft.winner = null;
+      draft.playing = true;
+    })
+    return nextState;
   }
   
   return state;
 };
 
 export const store = createStore(reducer);
+
+// store.subscribe(() => {
+//   console.log("Nouveau state:");
+//   console.log(store.getState());
+// });
